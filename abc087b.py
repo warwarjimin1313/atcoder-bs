64a6f3efc12b099838282ddd86087c190a1be2cd
AP19080 ワ　ワ　ウィン 　 <ap19080@azec.ac.jp>
2021-02-17 14:54:27 +0900

    modified: abc087b.py

--- abc087b.py
+++ abc087b.py
@@ -9,7 +9,7 @@ for n500 in range(0,a+1):
     for n100 in range(0,b+1):
         for n50 in range(0,c+1):
             amount = n500*500+n100*100+n50*50
-            print(n500,n100,n50,amount)
-            #if amount ==x:
-            #    num_combination += 1
+            #print(n500,n100,n50,amount)
+            if amount ==x:
+                num_combination += 1
 print(num_combination)
